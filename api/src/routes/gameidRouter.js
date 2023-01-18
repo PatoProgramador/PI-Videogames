d648b548772f57ad3246419a5965c746ccb007f7
Pato Programador <spartan117947@hotmail.com>
2023-01-18 18:18:23 -0500

    games controllers and routes ok

--- api/src/routes/gameidRouter.js
+++ api/src/routes/gameidRouter.js
@@ -7,7 +7,6 @@ router.get("/:id", async (req, res) => {
     const {id} = req.params;
     try {
         const game = await findGameById(id);
-        console.log(game)
         res.status(200).json(game);
     } catch (error) {
         res.status(400).json({error: error.message});
