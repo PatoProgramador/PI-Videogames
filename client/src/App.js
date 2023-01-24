6b0d4606ee37ebbbc5dd9299ab42ab74308ec039
PatoProgramador <spartan1171947@hotmail.com>
2023-01-23 22:57:44 -0500

    Rutas y components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Route } from "react-router-dom";
+import Landing from './components/landingPage/Landing';
+import Home from './components/Home/Home';
+import Detail from './components/Detail/Detail';
+import Form from './components/Form/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Route exact path="/" component={Landing}/>
+      <Route path="/Videogames" component={Home}/>
+      <Route path="/Detail/:id" component={Detail}/>
+      <Route path="/Form" component={Form}/>
     </div>
   );
 }
---
+++ client/src/components/landingPage/Landing.jsx
@@ -0,0 +1,9 @@
+const Landing =() => {
+    return (
+        <>
+            <h1>landing page</h1>
+        </>
+    )
+};
+
+export default Landing;
